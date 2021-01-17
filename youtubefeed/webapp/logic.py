490d26a9c8fc49c6a7810e843f59ffc245b03143
Alexis <af.allendes@outlook.com>
2021-01-17 13:43:12 -0300

    Update logic.py

--- youtubefeed/webapp/logic.py
+++ youtubefeed/webapp/logic.py
@@ -4,10 +4,10 @@ from datetime import datetime
 
 import requests
 
+BASE_URL = 'https://www.youtube.com'
+
 class YouTubeChannel:
     def __init__(self, url):
-        BASE_URL = 'https://www.youtube.com'
-        
         if any([
             url.startswith(BASE_URL + '/c/'),
             url.startswith(BASE_URL + '/channel/')
@@ -22,23 +22,29 @@ class YouTubeChannel:
         Requests and saves basic information of the channel.
         """
         response = requests.get(self.url)
-        content = response.content.decode('utf-8')
 
-        # Get RSS
-        re_pattern = r'\"rssUrl\":\"(?P<rss>https:\/\/www.youtube\.com/feeds\/videos\.xml\?channel_id\=\w*)",'
-        re_search = re.search(re_pattern, content)
-        self.rss = re_search.group('rss')
+        if response.status_code == '200':
+            content = response.content.decode('utf-8')
+
+            # Get UID and Title
+            re_pattern = r'\"channelId\":\"(?P<uid>\w*)",\"title\":\"(?P<title>.*)\",\"navigationEndpoint\"'
+            re_search = re.search(re_pattern, content)
+            self.uid = re_search.group('uid')
+            self.title = re_search.group('title')
+
+            # Get Avatar
+            re_pattern = r'\"avatar\":\{\"thumbnails\":\[\{\"url\":\"(?P<avatar>https:\/\/yt3.ggpht.com\/ytc\/[\w_-]*)=s'
+            re_search = re.search(re_pattern, content)
+            self.avatar = re_search.group('avatar')
+
+            # Construct Feed URL
+            self.rss = BASE_URL + '/feeds/videos.xml?channel_id=' + self.uid
+
+            # Construct cannonical channel URL (if required)
+            if BASE_URL + '/c/' in self.url:
+                self.url = BASE_URL + '/channel/' + self.uid
         
-        # Get UID and Title
-        re_pattern = r'\"channelId\":\"(?P<uid>\w*)",\"title\":\"(?P<title>.*)\",\"navigationEndpoint\"'
-        re_search = re.search(re_pattern, content)
-        self.uid = re_search.group('uid')
-        self.title = re_search.group('title')
-
-        # Get Avatar
-        re_pattern = r'\"avatar\":\{\"thumbnails\":\[\{\"url\":\"(?P<avatar>https:\/\/yt3.ggpht.com\/ytc\/[\w_-]*)=s'
-        re_search = re.search(re_pattern, content)
-        self.avatar = re_search.group('avatar')
+        raise ValueError('Invalid channel URL. Channel does not exist.\nURL: ' + self.url) 
     
     def update(self):
         """
@@ -54,18 +60,24 @@ class YouTubeChannel:
         response = requests.get(self.rss)
         content = response.content.decode('utf-8')
         root = ET.fromstring(content)
-        self.xml = root
-        root.findall(pre_append_namespace('entry'))
+        
+        self.xml = root # Just for Debug
+        
+        # Find all videos in this channels' feed
         entries = root.findall(pre_append_namespace('entry'))
-
         for entry in entries:
             self.feed.append({
+                # Get video UID
                 "uid": entry.find(pre_append_namespace('id')).text.replace('yt:video:', ''),
+                # Get video Title
                 "title": entry.find(pre_append_namespace('title')).text,
+                # Get video URL
                 "url": entry.find(pre_append_namespace('link')).get('href'),
+                # Get video published date
                 "published": datetime.strptime(
                     entry.find(pre_append_namespace('published')).text,
                     '%Y-%m-%dT%H:%M:%S%z'
                 ),
+                # Add video last time checked date
                 "last_check": today
             })
